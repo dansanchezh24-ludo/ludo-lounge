7a00daec00411269d72a42cef67fb3a4f28f06d6
Dan Sanchez <dan.sanchezh24@gmail.com>
2026-03-26 19:59:33 -0600

    Fix SPA paths and vercel.json for production

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // <--- Esto es clave para que los assets se carguen correctamente
+  base: './'  // <--- Muy importante para que los assets sean relativos
 })
