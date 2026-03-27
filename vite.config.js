dbb01e76458214024d35b0da3ef8893c9eab8566
Dan Sanchez <dan.sanchezh24@gmail.com>
2026-03-26 20:07:23 -0600

    Fix Vite config defineConfig + base path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './'  // <--- Muy importante para que los assets sean relativos
+  base: './', // ⚠️ paths relativos para producción
 })
