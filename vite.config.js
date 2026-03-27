dd89259c2a6321146b49ddac3ae3f247440d4dca
Dan Sanchez <dan.sanchezh24@gmail.com>
2026-03-26 20:12:51 -0600

    Fix Vite base path + vercel.json SPA

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // ⚠️ paths relativos para producción
+  base: './', // <- Esto hace que JS y CSS usen rutas relativas
 })
