d258c4bfed3a14cdf45b50d5fe2e5845af88f31d
Dan Sanchez <dan.sanchezh24@gmail.com>
2026-03-26 20:21:59 -0600

    Build para producción con rutas absolutas

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  base: './', // <- Esto hace que JS y CSS usen rutas relativas
+  base: '/',
+  plugins: [react()]
 })
