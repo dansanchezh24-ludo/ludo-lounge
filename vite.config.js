ca4bb506e3b277ca26830372b596f2f50f809f4d
Dan Sanchez <dan.sanchezh24@gmail.com>
2026-03-26 19:52:27 -0600

    Fix vercel.json y base path Vite

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './', // <- Esto hace que todos los paths en index.html sean relativos
   plugins: [react()],
-  build: {
-    outDir: 'dist'
-  }
+  base: './', // <--- Esto es clave para que los assets se carguen correctamente
 })
